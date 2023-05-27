4917183915f17a7258a5f7991c76022b09aadfb0
Kutay Özdür <93797241+printfHypna@users.noreply.github.com>
2023-05-27 15:20:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,10 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
     	
     	Login l = new Login();
     	l.setVisible(true);
-    	// Tree Main
-    	// Each node consists diseases and the symptoms of it
-    	
-    	
-    	LinkedList userList = new LinkedList();
-    	String username = l.getUsername();
-    	
-    	Nose n = new Nose(username);
-    	ArrayList<String> symptoms = new ArrayList();
-    	symptoms = n.getSelectedCheckBoxes(); 
-    	userList.add(username, symptoms);
-    	userList.display();
-    	
     }
 
    
