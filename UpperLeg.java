97dbb51831194214c2144e77d34d6156d6fbf7e5
Kutay Özdür <93797241+printfHypna@users.noreply.github.com>
2023-05-30 02:13:31 +0300

    Update UpperLeg.java

--- UpperLeg.java
+++ UpperLeg.java
@@ -1,7 +1,8 @@
-package arayüzgeliþtirme;
+package main;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -23,14 +24,18 @@ public class UpperLeg extends JFrame implements MouseListener {
 	JCheckBox chkBI = new JCheckBox("TENDERNESS");
 	
 	JCheckBox chkCG = new JCheckBox("WALKING DIFFICULTY");
-	JCheckBox chkCKS= new JCheckBox("BUGLE OR LUMP IN THE UPPER THIGH");
+	JCheckBox chkCKS= new JCheckBox("BULGE OR LUMP IN THE UPPER THIGH");
 
-	JCheckBox chkFP = new JCheckBox("PAIN OR DISCOMFORT ORESENT IN UPPER LEG");
+	JCheckBox chkFP = new JCheckBox("PAIN OR DISCOMFORT PRESENT IN UPPER LEG");
 	
 	JCheckBox chkLJ = new JCheckBox("STRENGHT DURING PYHSICAL ACTIVITY");
-	public UpperLeg() {
+	
+	private String username;
+	public UpperLeg(String username) {
+		
+		this.username = username;
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
-		setSize(400,700);
+		setSize(600,700);
 		
 		setLocationRelativeTo(null);
 		setTitle("Disease Diagnosis Program");
@@ -44,15 +49,39 @@ public class UpperLeg extends JFrame implements MouseListener {
 		add(new JLabel("(You can choose one or more options.)"));
 		add(pnlHairyArea);
 		add(btnSubmit);
+		
+		btnSubmit.addMouseListener(this);
 	
 	
 	
 	}
+
+	ArrayList<String> selectedCheckBoxes = new ArrayList<>();
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == btnSubmit) {
+			ArrayList<JCheckBox> allCheckBoxes = new ArrayList<>();
+			allCheckBoxes.add(chkIB); allCheckBoxes.add(chkITC);
+			allCheckBoxes.add(chkBI); allCheckBoxes.add(chkCG);allCheckBoxes.add(chkCKS);allCheckBoxes.add(chkFP);allCheckBoxes.add(chkLJ);
+			for (JCheckBox jCheckBox : allCheckBoxes) {
+				if (jCheckBox.isSelected()) {
+					selectedCheckBoxes.add(jCheckBox.getText());
+				}
+			}
+			if(selectedCheckBoxes.isEmpty() == false) {
+				Results r = new Results(username, selectedCheckBoxes);
+				r.setVisible(true);
+				dispose();
+			}
+		
+        } 
+		
 		// TODO Auto-generated method stub
 		
 	}
+	public ArrayList<String> getSelectedCheckBoxes() {
+        return selectedCheckBoxes;
+    }
 
 	@Override
 	public void mousePressed(MouseEvent e) {
